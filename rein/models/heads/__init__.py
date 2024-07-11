801b52f4c0a354630d004f753910bd46a3eb85cd
yuanczx <yuanczx@foxmail.com>
2024-07-11 20:47:09 +0800

    add: frozen backbone SSFMask2FormerHead

--- rein/models/heads/__init__.py
+++ rein/models/heads/__init__.py
@@ -1,3 +1,4 @@
 from .rein_mask2former import ReinMask2FormerHead
-from .ssf_mask2former import SSFMask2FormerHead
-__all__ = ["ReinMask2FormerHead","SSFMask2FormerHead"]
+from .rein_ssfmask2former import SSFMask2FormerHead
+from .frozenbackbone_ssfmask2former import FB_SSFMask2FormerHead
+__all__ = ["ReinMask2FormerHead","SSFMask2FormerHead","FB_SSFMask2FormerHead"]

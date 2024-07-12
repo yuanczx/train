d203d112fb34a0ef8b56091d2347e9a3a58a47d3
yuanczx <yuanczx@foxmail.com>
2024-07-12 10:21:31 +0800

    add: configs

--- configs/frozen_vfms/dinov2-L_ssfmask2former.py
+++ configs/frozen_vfms/dinov2-L_ssfmask2former.py
@@ -1,12 +1,28 @@
-_base_= [
+_base_ = [
     "../_base_/datasets/dg_gta_512x512.py",
-    "../_base_/models/dinov2_ssfmask2former.py",
-    "../_base_/schedules/schedule_40k.py",
-    "../_base_/default_runtime.py"
+    "../_base_/default_runtime.py",
+    "../_base_/models/dinov2_mask2former.py",
 ]
+model = dict(type="FrozenBackboneEncoderDecoder",decode_head=dict(type="FB_SSFMask2FormerHead"))
+train_pipeline = [
+    dict(type="LoadImageFromFile"),
+    dict(type="LoadAnnotations"),
+    dict(
+        type="RandomChoiceResize",
+        scales=[int(512 * x * 0.1) for x in range(5, 21)],
+        resize_type="ResizeShortestEdge",
+        max_size=2048,
+    ),
+    dict(type="RandomCrop", crop_size={{_base_.crop_size}}, cat_max_ratio=0.75),
+    dict(type="RandomFlip", prob=0.5),
+    dict(type="PhotoMetricDistortion"),
+    dict(type="PackSegInputs"),
+]
+train_dataloader = dict(batch_size=1, dataset=dict(pipeline=train_pipeline))
 
+# AdamW optimizer, no weight decay for position embedding & layer norm
+# in backbone
 embed_multi = dict(lr_mult=1.0, decay_mult=0.0)
-train_dataloader = dict(batch_size=1)
 optim_wrapper = dict(
     constructor="PEFTOptimWrapperConstructor",
     optimizer=dict(
@@ -14,11 +30,10 @@ optim_wrapper = dict(
     ),
     paramwise_cfg=dict(
         custom_keys={
-            "norm": dict(decay_mult=0.0),
             "query_embed": embed_multi,
+            "query_feat": embed_multi,
             "level_embed": embed_multi,
-            "learnable_tokens": embed_multi,
-            "reins.scale": embed_multi,
+            "norm": dict(decay_mult=0.0),
         },
         norm_decay_mult=0.0,
     ),
@@ -26,5 +41,19 @@ optim_wrapper = dict(
 param_scheduler = [
     dict(type="PolyLR", eta_min=0, power=0.9, begin=0, end=40000, by_epoch=False)
 ]
+
+train_cfg = dict(type="IterBasedTrainLoop", max_iters=40000, val_interval=10000)
+val_cfg = dict(type="ValLoop")
+test_cfg = dict(type="TestLoop")
+default_hooks = dict(
+    timer=dict(type="IterTimerHook"),
+    logger=dict(type="LoggerHook", interval=50, log_metric_by_epoch=False),
+    param_scheduler=dict(type="ParamSchedulerHook"),
+    checkpoint=dict(
+        type="CheckpointHook", by_epoch=False, interval=4000, max_keep_ckpts=3
+    ),
+    sampler_seed=dict(type="DistSamplerSeedHook"),
+    visualization=dict(type="SegVisualizationHook"),
+)
 
 work_dir = './work_dirs/dinov2_gta2others_ssfmask2former'

611117eec25e4c6f442b18734b285ade4be439dc
yuanczx <yuanczx@foxmail.com>
2024-07-12 10:47:15 +0800

    add: SSFMask2Former

--- rein/models/heads/__init__.py
+++ rein/models/heads/__init__.py
@@ -1,4 +1,4 @@
 from .rein_mask2former import ReinMask2FormerHead
 from .rein_ssfmask2former import SSFMask2FormerHead
-from .frozenbackbone_ssfmask2former import FB_SSFMask2FormerHead
-__all__ = ["ReinMask2FormerHead","SSFMask2FormerHead","FB_SSFMask2FormerHead"]
+from .frozenbackbone_ssfmask2former import FB_SSFMask2FormerHead, FB_MSSFMask2FormerHead
+__all__ = ["ReinMask2FormerHead","SSFMask2FormerHead","FB_SSFMask2FormerHead","FB_MSSFMask2FormerHead"]
--- rein/models/heads/frozenbackbone_ssfmask2former.py
+++ rein/models/heads/frozenbackbone_ssfmask2former.py
@@ -28,6 +28,88 @@ def ssf_ada(x, scale, shift):
         raise ValueError('the input tensor shape does not match the shape of the scale factor.')
     
     
+class SSF(nn.Module):
+    def __init__(self,dim):
+        super().__init__(self)
+        self.dim = dim
+        self.scale, self.shift = init_ssf_scale_shift(dim)
+        
+    def forward(self,x):
+        return ssf_ada(x,self.scale,self.shift)
+        
+    
+@MODELS.register_module()
+class FB_MSSFMask2FormerHead(Mask2FormerHead):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        feat_channels = kwargs["feat_channels"]
+        self.models = []
+        for _ in range(self.num_transformer_decoder_layers):
+            self.models.append(SSF(feat_channels))
+
+    def forward(self, x: List[Tensor],
+                batch_data_samples: SampleList) -> Tuple[List[Tensor]]:
+        batch_size = x[0].shape[0]
+        mask_features, multi_scale_memorys = self.pixel_decoder(x)
+        # multi_scale_memorys (from low resolution to high resolution)
+        decoder_inputs = []
+        decoder_positional_encodings = []
+        for i in range(self.num_transformer_feat_level):
+            decoder_input = self.decoder_input_projs[i](multi_scale_memorys[i])
+            # shape (batch_size, c, h, w) -> (batch_size, h*w, c)
+            decoder_input = decoder_input.flatten(2).permute(0, 2, 1)
+            level_embed = self.level_embed.weight[i].view(1, 1, -1)
+            decoder_input = decoder_input + level_embed
+            # shape (batch_size, c, h, w) -> (batch_size, h*w, c)
+            mask = decoder_input.new_zeros(
+                (batch_size, ) + multi_scale_memorys[i].shape[-2:],
+                dtype=torch.bool)
+            decoder_positional_encoding = self.decoder_positional_encoding(
+                mask)
+            decoder_positional_encoding = decoder_positional_encoding.flatten(
+                2).permute(0, 2, 1)
+            decoder_inputs.append(decoder_input)
+            decoder_positional_encodings.append(decoder_positional_encoding)
+        # shape (num_queries, c) -> (batch_size, num_queries, c)
+        query_feat = self.query_feat.weight.unsqueeze(0).repeat(
+            (batch_size, 1, 1))
+        query_embed = self.query_embed.weight.unsqueeze(0).repeat(
+            (batch_size, 1, 1))
+
+        cls_pred_list = []
+        mask_pred_list = []
+        cls_pred, mask_pred, attn_mask = self._forward_head(
+            query_feat, mask_features, multi_scale_memorys[0].shape[-2:])
+        cls_pred_list.append(cls_pred)
+        mask_pred_list.append(mask_pred)
+
+        for i in range(self.num_transformer_decoder_layers):
+            level_idx = i % self.num_transformer_feat_level
+            # if a mask is all True(all background), then set it all False.
+            mask_sum = (attn_mask.sum(-1) != attn_mask.shape[-1]).unsqueeze(-1)
+            attn_mask = attn_mask & mask_sum
+            # cross_attn + self_attn
+            layer = self.transformer_decoder.layers[i]
+            query_feat = layer(
+                query=query_feat,
+                key=decoder_inputs[level_idx],
+                value=decoder_inputs[level_idx],
+                query_pos=query_embed,
+                key_pos=decoder_positional_encodings[level_idx],
+                cross_attn_mask=attn_mask,
+                query_key_padding_mask=None,
+                # here we do not apply masking on padded region
+                key_padding_mask=None)
+            query_feat = self.models[i](query_feat)
+            cls_pred, mask_pred, attn_mask = self._forward_head(
+                query_feat, mask_features, multi_scale_memorys[
+                    (i + 1) % self.num_transformer_feat_level].shape[-2:])
+
+            cls_pred_list.append(cls_pred)
+            mask_pred_list.append(mask_pred)
+
+        return cls_pred_list, mask_pred_list
+    
 @MODELS.register_module()
 class FB_SSFMask2FormerHead(Mask2FormerHead):
     def __init__(self, **kwargs):
